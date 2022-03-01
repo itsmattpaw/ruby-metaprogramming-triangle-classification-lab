507ee3173dba526dc885fbee44f6c4fda5ebd479
itsmattpaw <itsmattpaw@gmail.com>
2022-02-28 20:34:31 -0500

    commit tree 7fce88c1d24a53f6289f111a172aac3e9b3c2b6b

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c   
+  def initialize(a,b,c)
+    @a = a
+    @b = b   
+    @c = c 
+    valid? 
+  end
+  def valid?
+    if @a <= 0 || @b <= 0 || @c <= 0
+      raise TriangleError
+    end
+    if ((@a+@b)<=@c) || ((@a+@c)<=@b) || ((@c+@b)<=@a)
+      raise TriangleError
+    end
+  end
+  def kind
+    if (@a == @b && @b == @c)
+      return :equilateral
+    elsif ((@a == @b && @a != @c) || (@b == @c && @a != @c) || (@a == @c && @b != @c))
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
